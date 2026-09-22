db9f57d361f605a90826f2a7a9716f0a830c5b5c
Rtyuiop699 <swbr8519@gmail.com>
2026-09-22 08:45:08 +0300

    Add ProcessedImagesActivity for displaying processed images

---
+++ app/src/main/java/com/saber/myapp/ProcessedImagesActivity.kt
@@ -0,0 +1,13 @@
+package com.saber.myapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProcessedImagesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_processed_images)
+    }
+}
